11ece07c34d8647f21ade2e2f35e3db6e47a653c
PC <PC@DESKTOP-FPVCC1C>
2023-10-13 22:28:28 +0700

    Add project files.

---
+++ ShopMeoSolution.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopMeoSolution.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
